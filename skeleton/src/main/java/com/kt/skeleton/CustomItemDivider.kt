d61b3a8aa9294f2188a78dc943d5a267691cf06a
tuandv4 <dvt1405@gmail.com>
2022-10-24 01:10:40 +0700

    Add custom color for divider

--- skeleton/src/main/java/com/kt/skeleton/CustomItemDivider.kt
+++ skeleton/src/main/java/com/kt/skeleton/CustomItemDivider.kt
@@ -2,8 +2,12 @@ package com.kt.skeleton
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Rect
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
@@ -13,9 +17,11 @@ open class CustomItemDivider(
     val context: Context,
     val orientation: Int,
     private val margin: Float = 0f,
-    private val useDefault: Boolean = false
+    private val useDefault: Boolean = false,
+    private val customColor: Int = Color.parseColor("#20FFFFFF")
 ) : DividerItemDecoration(context, orientation) {
-    private var customDrawable: Drawable? = ContextCompat.getDrawable(context, R.drawable.line_divider)!!
+    private var customDrawable: Drawable? =
+        ContextCompat.getDrawable(context, R.drawable.line_divider)!!
 
     override fun setDrawable(drawable: Drawable) {
         super.setDrawable(drawable)
@@ -44,7 +50,20 @@ open class CustomItemDivider(
             mDrawable.isAccessible = true
             mFieldBounds.isAccessible = true
             val mBounds = mFieldBounds.get(this) as Rect
-            val mDivider = if (useDefault) mDrawable.get(this) as Drawable else customDrawable!!
+            val mDivider =
+                if (useDefault) mDrawable.get(this) as Drawable else customDrawable!!.apply {
+                    when (this) {
+                        is ShapeDrawable -> {
+                            this.paint.color = customColor
+                        }
+                        is GradientDrawable -> {
+                            this.setColor(customColor)
+                        }
+                        is ColorDrawable -> {
+                            this.color = customColor
+                        }
+                    }
+                }
             val childCount = parent.childCount
             for (i in 0 until childCount) {
                 val child = parent.getChildAt(i)
