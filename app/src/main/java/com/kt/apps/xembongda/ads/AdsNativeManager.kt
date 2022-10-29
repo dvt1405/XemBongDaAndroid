1a70c34e1df4250c43172cf9968d6deae6e533f2
tuandv4 <dvt1405@gmail.com>
2022-10-30 00:48:10 +0700

    Improve load native ads ux

--- app/src/main/java/com/kt/apps/xembongda/ads/AdsNativeManager.kt
+++ app/src/main/java/com/kt/apps/xembongda/ads/AdsNativeManager.kt
@@ -1,20 +1,26 @@
 package com.kt.apps.xembongda.ads
 
 import android.content.Context
-import android.util.Log
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdLoader
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.VideoOptions
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.nativead.NativeAd
 import com.google.android.gms.ads.nativead.NativeAdOptions
 import com.kt.apps.xembongda.R
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.Inject
 
 class AdsNativeManager @Inject constructor(context: Context) {
 
+    @FunctionalInterface
+    interface Observer {
+        fun onReceiveAds(nativeAd: NativeAd)
+    }
+
+    private val compositeDisposable by lazy { CompositeDisposable() }
     private val videoOptions by lazy {
         VideoOptions.Builder()
             .setStartMuted(true)
@@ -29,7 +35,6 @@ class AdsNativeManager @Inject constructor(context: Context) {
             .build()
     }
 
-    var onLoadDone: ((ad: NativeAd) -> Unit)? = null
 
     private val nativeAdsListener by lazy {
         object : AdListener() {
@@ -47,48 +52,68 @@ class AdsNativeManager @Inject constructor(context: Context) {
         }
     }
 
+    private val nativeAdListener by lazy {
+        NativeAd.OnNativeAdLoadedListener {
+            synchronized(listAds) {
+                listAds.add(it)
+            }
+            emitter?.onNext(it)
+        }
+    }
+
+
     private val addLoader by lazy {
         AdLoader.Builder(context, context.getString(R.string.ad_mod_native_id))
-            .forNativeAd {
-                synchronized(listAds) {
-                    Log.e("TAG", "HasVideo: ${it.mediaContent?.hasVideoContent()}")
-                    listAds.add(it.apply {
-                        if (it.mediaContent?.mainImage == null) {
-                            Log.e("TAG", "mainImage: NUll")
-                            it.mediaContent?.mainImage = it.icon?.drawable
-                        }
-                    })
-                    this.onLoadDone?.invoke(it)
-                }
-            }
+            .forNativeAd(nativeAdListener)
             .withNativeAdOptions(nativeOptions)
             .withAdListener(nativeAdsListener)
             .build()
     }
 
+
     val listAds by lazy {
-        ArrayDeque<NativeAd>()
+        ConcurrentLinkedQueue<NativeAd>()
     }
 
     private val isLoading by lazy {
         AtomicBoolean(false)
     }
 
-    fun preloadNativeAds(onLoadDone: ((ad: NativeAd) -> Unit)? = null) {
-        this.onLoadDone = onLoadDone
-        if (!addLoader.isLoading) {
-            listAds.clear()
-            addLoader.loadAds(
-                AdRequest.Builder()
-                    .build(), 10
-            )
-        }
+    private val adRequest by lazy {
+        AdRequest.Builder()
+            .build()
+
+    }
+
+    private var emitter: ObservableEmitter<NativeAd>? = null
+    fun preloadNativeAds() {
+        if (addLoader.isLoading) return
+        compositeDisposable.add(
+            Observable.create {
+                emitter = it
+                if (!addLoader.isLoading) {
+                    addLoader.loadAds(adRequest, 10)
+                }
+            }
+                .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    concurrentLinkedQueue.last()?.onReceiveAds(it)
+                }, {
+
+                }, {
+
+                })
+        )
+
     }
 
     fun getLastItem(): NativeAd? {
         return try {
             val lastItem = if (listAds.isNotEmpty()) {
-                listAds.removeLast()
+                val last = listAds.last()
+                listAds.remove(listAds.last())
+                last
             } else {
                 null
             }
@@ -99,6 +124,20 @@ class AdsNativeManager @Inject constructor(context: Context) {
         } catch (e: Exception) {
             null
         }
+    }
+
+
+
+    private val concurrentLinkedQueue by lazy {
+        ConcurrentLinkedQueue<Observer>()
+    }
+
+    fun addObserver(observer: Observer) {
+        concurrentLinkedQueue.add(observer)
+    }
+
+    fun unregister(observer: Observer) {
+        concurrentLinkedQueue.remove(observer)
     }
 
 }
