6bae9c3917f815762027c864c6532bd22b6d1c3b
tuandv4 <dvt1405@gmail.com>
2022-11-01 02:37:25 +0700

    Register volume change

--- app/src/main/java/com/kt/apps/xembongda/App.kt
+++ app/src/main/java/com/kt/apps/xembongda/App.kt
@@ -1,16 +1,22 @@
 package com.kt.apps.xembongda
 
+import android.app.Activity
+import android.app.Application.ActivityLifecycleCallbacks
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.kt.apps.xembongda.ads.AdsConfigManager
 import com.kt.apps.xembongda.ads.AdsNativeManager
 import com.kt.apps.xembongda.ads.RewardedAdsManager
 import com.kt.apps.xembongda.di.DaggerAppComponents
 import com.kt.apps.xembongda.di.DaggerBaseComponents
+import com.kt.apps.xembongda.receiver.VolumeChangeReceiver
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import javax.inject.Inject
 
-class App : DaggerApplication() {
+class App : DaggerApplication(), ActivityLifecycleCallbacks {
     private val _baseComponents by lazy {
         DaggerBaseComponents.builder()
             .application(this)
@@ -69,6 +75,32 @@ class App : DaggerApplication() {
     companion object {
         private lateinit var app: App
         fun get() = app
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        activity.registerReceiver(VolumeChangeReceiver.getInstance(), IntentFilter().apply {
+            addAction("android.media.VOLUME_CHANGED_ACTION")
+            addAction(Intent.ACTION_MEDIA_BUTTON)
+        })
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        unregisterReceiver(VolumeChangeReceiver.getInstance())
     }
 
 }
