e901a26cb2825b5b3d9232fd30955f0609a5f0b6
tuandv4 <dvt1405@gmail.com>
2022-11-01 02:39:41 +0700

    Add display name based on email or userid

--- base/src/main/java/com/kt/apps/xembongda/utils/firebase/FirebaseUtils.kt
+++ base/src/main/java/com/kt/apps/xembongda/utils/firebase/FirebaseUtils.kt
@@ -6,7 +6,11 @@ import com.kt.apps.xembongda.model.UserDTO
 fun FirebaseUser?.toUserDto(): UserDTO {
     if (this == null) return UserDTO("", "", null, false, "")
     return UserDTO(
-        name = displayName ?: "",
+        name = when {
+            !displayName.isNullOrEmpty() -> displayName!!
+            !email.isNullOrEmpty() -> email!!.trim().substring(0, email!!.indexOf("@"))
+            else -> uid ?: ""
+        },
         email = email ?: "",
         photoUrl = photoUrl,
         emailVerified = isEmailVerified,
