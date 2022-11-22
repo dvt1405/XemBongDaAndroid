a2e9af7a65b00cd706a101d7d7ab78ed9254259b
tuandv4 <dvt1405@gmail.com>
2022-11-22 20:03:39 +0700

    Add custom exception

--- base/src/main/java/com/kt/apps/xembongda/exceptions/ErrorCode.kt
+++ base/src/main/java/com/kt/apps/xembongda/exceptions/ErrorCode.kt
@@ -2,9 +2,13 @@ package com.kt.apps.xembongda.exceptions
 
 object ErrorCode {
 
+    const val CONNECT_TIMEOUT = 4410
+
     const val REQUIRED_LOGIN = 4403
     const val UN_AUTHENTICATE = 4401
 
     const val COMMENT_OVER_LOAD = 4402
     const val COMMENT_INVALID = 4401
+
+    const val UN_EXPECTED_ERROR = 4999
 }
